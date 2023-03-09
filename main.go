c838e46d2a5a124c574f823d2c84ba6d458a9d0e
tripg <phamtri2895@gmail.com>
2023-03-09 10:39:31 +0700

    process file by chunks

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"crypto/md5"
 	"encoding/hex"
+	"errors"
 	"fmt"
 	"github.com/gabriel-vasile/mimetype"
 	"github.com/google/uuid"
@@ -28,11 +29,14 @@ type ChunkedUploadRequest struct {
 }
 
 type ChunkedUpload struct {
-	uploadID  uuid.UUID
-	fPath     string
-	mimeType  string
-	chunkSize int64
-	nWorker   int
+	uploadID    uuid.UUID
+	fPath       string
+	mimeType    string
+	chunkSize   int64
+	nWorker     int
+	ctx         context.Context
+	cancel      context.CancelFunc
+	handlerFunc func(ChunkedUploadRequest) error
 
 	// internally used
 	success int
@@ -40,7 +44,6 @@ type ChunkedUpload struct {
 	total   int
 	cUpload chan ChunkedUploadRequest
 	cErr    chan error
-	ctx     context.Context
 }
 
 func NewChunkedUpload(fPath string, opts ...func(*ChunkedUpload)) (*ChunkedUpload, error) {
@@ -64,6 +67,16 @@ func NewChunkedUpload(fPath string, opts ...func(*ChunkedUpload)) (*ChunkedUploa
 
 	if c.uploadID == uuid.Nil {
 		c.uploadID = uuid.New()
+	}
+
+	if c.ctx == nil {
+		ctx, cancel := context.WithCancel(context.Background())
+		c.ctx = ctx
+		c.cancel = cancel
+	}
+
+	if c.handlerFunc == nil {
+		return nil, errors.New("handler function to process chunks is required")
 	}
 
 	return c, nil
@@ -100,34 +113,41 @@ func WithUploadID(id uuid.UUID) func(*ChunkedUpload) {
 	}
 }
 
-func WithCtx(ctx context.Context) func(*ChunkedUpload) {
+func WithCtx(ctx context.Context, cancelFunc context.CancelFunc) func(*ChunkedUpload) {
 	return func(c *ChunkedUpload) {
 		c.ctx = ctx
+		c.cancel = cancelFunc
+	}
+}
+
+func WithHandlerFunc(f func(ChunkedUploadRequest) error) func(upload *ChunkedUpload) {
+	return func(c *ChunkedUpload) {
+		c.handlerFunc = f
 	}
 }
 
 func main() {
 	fPath := "/home/tripg/workspace/OS-2.tiff"
 
-	parts, err := getNumberOfChunk(fPath)
+	ctx, cancel := context.WithCancel(context.Background())
+
+	cu, err := NewChunkedUpload(fPath, WithCtx(ctx, cancel), WithNWorkers(4), WithUploadID(uuid.New()), WithHandlerFunc(func(request ChunkedUploadRequest) error {
+		fmt.Println(request.Part, len(request.Content), request.Complete)
+		return nil
+	}))
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	fmt.Println(parts)
 
-	mtype, err := mimetype.DetectFile(fPath)
-	fmt.Println(mtype.String(), mtype.Extension())
+	defer cu.cancel()
 
-}
-
-func getNumberOfChunk(filePath string) (uint64, error) {
-	f, err := os.Stat(filePath)
+	err = cu.chunkedUploadHandler()
 	if err != nil {
-		return 0, err
+		fmt.Println(err)
+		return
 	}
 
-	return uint64(math.Ceil(float64(f.Size()) / float64(DefaultChunkSize))), nil
 }
 
 func calChunkMD5(chunk []byte) string {
@@ -150,6 +170,7 @@ func (cu *ChunkedUpload) producer() {
 	}
 
 	nChunks := cu.getNChunks(fInfo)
+	cu.total = int(nChunks)
 
 	for i := uint64(0); i < nChunks; i++ {
 		select {
@@ -159,7 +180,7 @@ func (cu *ChunkedUpload) producer() {
 		default:
 		}
 
-		partSize := int(math.Min(DefaultChunkSize, float64(fInfo.Size()-int64(i*DefaultChunkSize))))
+		partSize := int(math.Min(float64(cu.chunkSize), float64(fInfo.Size()-int64(i)*cu.chunkSize)))
 		partBuffer := make([]byte, partSize)
 		_, err := f.Read(partBuffer)
 		if err != nil {
@@ -168,9 +189,7 @@ func (cu *ChunkedUpload) producer() {
 				return
 			}
 		}
-
-		checkSum := md5.Sum(partBuffer)
-		md5Hash := hex.EncodeToString(checkSum[:])
+		md5Hash := calChunkMD5(partBuffer)
 
 		chunkedReq := ChunkedUploadRequest{
 			UploadID: cu.uploadID,
@@ -197,111 +216,42 @@ func (cu *ChunkedUpload) consumer(idx int, wg *sync.WaitGroup) {
 
 		chunkedReq, ok := <-cu.cUpload
 		if ok {
-			fmt.Println("req", chunkedReq.Part)
+			err := cu.handlerFunc(chunkedReq)
+			if err != nil {
+				cu.failure++
+				cu.cErr <- err
+				cu.cancel()
+				return
+			} else {
+				cu.success++
+			}
 		} else {
 			return
 		}
 	}
 }
 
-//func (cu *ChunkedUpload) chunkedUploadHandler(uploadID uuid.UUID, filePath string) {
-//
-//	var wg2 sync.WaitGroup
-//	cUpload := make(chan chunkedUploadRequest, uploadConfig.Concurrent)
-//
-//	ctx, cancel := context.WithCancel(context.Background())
-//	defer cancel()
-//
-//	go chunkGenerator(cUpload, ctx, uploadID, filePath)
-//
-//	for i := 1; i <= uploadConfig.Concurrent; i++ {
-//		wg2.Add(1)
-//		go chunkConsumer(i, cUpload, &wg2, filePath, ctx)
-//	}
-//
-//	wg2.Wait()
-//}
+func (cu *ChunkedUpload) chunkedUploadHandler() error {
 
-//func chunkConsumer(idx int, cUpload chan ChunkedUploadRequest, wg *sync.WaitGroup, filePath string, ctx context.Context) {
-//	defer wg.Done()
-//	for {
-//		select {
-//		case <-ctx.Done():
-//			fmt.Println("Worker", idx, "encountered an error")
-//			return
-//		default:
-//		}
-//
-//		chunkedReq, ok := <-cUpload
-//		if ok {
-//			_, code, err := shared.LabClient.UploadStudyByChunk(uploadConfig.Destination, chunkedReq)
-//			if code != http.StatusCreated && code != http.StatusOK {
-//				logger.Info(strings.Join([]string{fmt.Sprintf("%s (chunk %d)", filePath, chunkedReq.Part), "0", Err, err.Error()}, ","))
-//			} else {
-//				if chunkedReq.Complete {
-//					uploadSuccess++
-//				}
-//				logger.Info(strings.Join([]string{fmt.Sprintf("%s (chunk %d)", filePath, chunkedReq.Part), "0", Ok}, ","))
-//			}
-//		} else {
-//			return
-//		}
-//	}
-//}
-//
-//func chunkedUploadHandler(uploadID uuid.UUID, filePath string) {
-//
-//	var wg2 sync.WaitGroup
-//	cUpload := make(chan chunkedUploadRequest, uploadConfig.Concurrent)
-//
-//	ctx, cancel := context.WithCancel(context.Background())
-//	defer cancel()
-//
-//	go chunkGenerator(cUpload, ctx, uploadID, filePath)
-//
-//	for i := 1; i <= uploadConfig.Concurrent; i++ {
-//		wg2.Add(1)
-//		go chunkConsumer(i, cUpload, &wg2, filePath, ctx)
-//	}
-//
-//	wg2.Wait()
-//}
+	var wg sync.WaitGroup
 
-//func chunkProducer(ctx context.Context, cUpload chan ChunkedUploadRequest, uploadID uuid.UUID, filePath string) {
-//	defer close(cUpload)
-//	f, _ := os.Open(filePath)
-//	defer f.Close()
-//	fInfo, _ := f.Stat()
-//
-//	totalPartsNum, _ := getNumberOfChunk(filePath)
-//
-//	for i := uint64(0); i < totalPartsNum; i++ {
-//		select {
-//		case <-ctx.Done():
-//			fmt.Println("Upload producer encountered an error")
-//			return
-//		default:
-//		}
-//
-//		partSize := int(math.Min(DefaultChunkSize, float64(fInfo.Size()-int64(i*DefaultChunkSize))))
-//		partBuffer := make([]byte, partSize)
-//
-//		f.Read(partBuffer)
-//
-//		checkSum := md5.Sum(partBuffer)
-//		md5Hash := hex.EncodeToString(checkSum[:])
-//
-//		chunkedReq := ChunkedUploadRequest{
-//			UploadID: uploadID,
-//			Part:     int(i) + 1,
-//			MD5:      md5Hash,
-//			Content:  partBuffer,
-//		}
-//
-//		if i == totalPartsNum-1 {
-//			chunkedReq.Complete = true
-//		}
-//
-//		cUpload <- chunkedReq
-//	}
-//}
+	go cu.producer()
+
+	for i := 1; i <= cu.nWorker; i++ {
+		wg.Add(1)
+		go cu.consumer(i, &wg)
+	}
+
+	wg.Wait()
+	close(cu.cErr)
+
+	for {
+		err, ok := <-cu.cErr
+		if ok {
+			return err
+		} else {
+			break
+		}
+	}
+	return nil
+}
