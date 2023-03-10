e028cceb0572c26ee6487500a08cb62beae08381
tripg <phamtri2895@gmail.com>
2023-03-10 14:37:56 +0700

    update test

--- gochunk_test.go
+++ gochunk_test.go
@@ -1,21 +1,28 @@
 package gochunk
 
 import (
+	"bytes"
 	"context"
 	"errors"
+	"fmt"
 	"github.com/google/uuid"
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
 	"github.com/stretchr/testify/assert"
+	"strconv"
+	"strings"
 	"testing"
 )
 
 func TestNewChunkedUpload(t *testing.T) {
 	assert := assert.New(t)
 	fPath := "/home/tripg/workspace/OS-2.tiff"
+	fPath = "/home/tripg/workspace/ubuntu.iso"
 
 	ctx, cancel := context.WithCancel(context.Background())
 
 	cu, err := NewChunkedUpload(fPath, WithCtx(ctx, cancel), WithNWorkers(4), WithUploadID(uuid.New()), WithHandlerFunc(func(request ChunkedUploadRequest) error {
-		//fmt.Println(request.Part, len(request.Content), request.Complete)
+		fmt.Println(request.Part, len(request.Content), request.Complete)
 		return nil
 	}))
 	assert.NoError(err)
@@ -62,4 +69,99 @@ func BenchmarkNewChunkedUpload(b *testing.B) {
 			return
 		}
 	}
+}
+
+func TestChunkedUpload_ChunkHandler(t *testing.T) {
+	fPath := "/home/tripg/workspace/OS-2.tiff"
+	fPath = "/home/tripg/workspace/ubuntu.iso"
+	bucketName := "chunked-upload"
+
+	assert := assert.New(t)
+
+	minioClient, err := minio.New(
+		"localhost:19000",
+		&minio.Options{
+			Creds: credentials.NewStaticV4("vinlab", "ax9F6q!8V@FdS6$%", ""),
+		})
+	assert.NoError(err)
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	cu, err := NewChunkedUpload(fPath, WithCtx(ctx, cancel), WithChunkSize(104_857_600), WithNWorkers(40), WithUploadID(uuid.New()), WithHandlerFunc(func(request ChunkedUploadRequest) error {
+		_, err = minioClient.PutObject(
+			context.Background(),
+			bucketName,
+			strings.ReplaceAll(request.UploadID.String(), "-", ".")+"."+strconv.Itoa(request.Part),
+			bytes.NewReader(request.Content),
+			int64(len(request.Content)),
+			minio.PutObjectOptions{},
+		)
+		assert.NoError(err)
+		return nil
+	}))
+	assert.NoError(err)
+
+	err = cu.ChunkHandler()
+	assert.NoError(err)
+}
+
+func TestChunkedUpload_ChunkHandler_2(t *testing.T) {
+	//fPath := "/home/tripg/workspace/OS-2.tiff"
+	//fPath = "/home/tripg/workspace/ubuntu.iso"
+
+	fPaths := []string{
+		"/home/tripg/workspace/OS-2.tiff",
+		"/home/tripg/workspace/ubuntu.iso",
+	}
+
+	bucketName := "chunked-upload"
+
+	assert := assert.New(t)
+
+	minioClient, err := minio.New(
+		"localhost:19000",
+		&minio.Options{
+			Creds: credentials.NewStaticV4("vinlab", "ax9F6q!8V@FdS6$%", ""),
+		})
+	assert.NoError(err)
+
+	for _, fPath := range fPaths {
+		fmt.Println("Process", fPath)
+		ctx, cancel := context.WithCancel(context.Background())
+		cu, err := NewChunkedUpload(fPath, WithCtx(ctx, cancel), WithNWorkers(20), WithUploadID(uuid.New()), WithHandlerFunc(func(request ChunkedUploadRequest) error {
+			fmt.Println("processing", request.Part)
+			_, err = minioClient.PutObject(
+				context.Background(),
+				bucketName,
+				strings.ReplaceAll(request.UploadID.String(), "-", ".")+"."+strconv.Itoa(request.Part),
+				bytes.NewReader(request.Content),
+				int64(len(request.Content)),
+				minio.PutObjectOptions{},
+			)
+			assert.NoError(err)
+			return nil
+		}))
+		assert.NoError(err)
+
+		err = cu.ChunkHandler()
+		assert.NoError(err)
+	}
+
+	//cu, err := NewChunkedUpload(fPath, WithCtx(ctx, cancel), WithNWorkers(470), WithUploadID(uuid.New()), WithHandlerFunc(func(request ChunkedUploadRequest) error {
+	//	fmt.Println("processing", request.Part)
+	//	_, err = minioClient.PutObject(
+	//		context.Background(),
+	//		bucketName,
+	//		strings.ReplaceAll(request.UploadID.String(), "-", ".")+"."+strconv.Itoa(request.Part),
+	//		bytes.NewReader(request.Content),
+	//		int64(len(request.Content)),
+	//		minio.PutObjectOptions{},
+	//	)
+	//	assert.NoError(err)
+	//	return nil
+	//}))
+	//assert.NoError(err)
+	//
+	//err = cu.ChunkHandler()
+	//assert.NoError(err)
 }
